0677959fad3977c202eb637b966145a2dc0f6bb0
dymafr <32801166+dymafr@users.noreply.github.com>
2022-12-05 13:23:15 +0100

    1 file modified

--- src/app/exemple.ts
+++ src/app/exemple.ts
@@ -1,25 +1,25 @@
-import { Component } from "@angular/core";
-import { MatDialog } from "@angular/material/dialog";
-import { DialogComponent } from "./dialog/dialog";
+import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogComponent } from './dialog/dialog';
 
 @Component({
-  selector: "app-exemple-component",
-  templateUrl: "exemple.html",
-  styleUrls: ["exemple.scss"]
+  selector: 'app-exemple-component',
+  templateUrl: 'exemple.html',
+  styleUrls: ['exemple.scss'],
 })
 export class ExempleComponent {
-  public plat: string;
-  public nom: string = "Bob";
+  public plat!: string;
+  public nom: string = 'Bob';
 
   constructor(public dialog: MatDialog) {}
 
   openDialog(): void {
     let dialogRef = this.dialog.open(DialogComponent, {
-      width: "250px",
-      data: { nom: this.nom, plat: this.plat }
+      width: '250px',
+      data: { nom: this.nom, plat: this.plat },
     });
 
-    dialogRef.afterClosed().subscribe(result => {
+    dialogRef.afterClosed().subscribe((result) => {
       this.plat = result;
     });
   }
